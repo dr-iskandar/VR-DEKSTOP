8c35cf143cd500267fc753ebd64fae3d2f630f33
dr-iskandar <drachmat.iskandar@gmail.com>
2020-02-13 04:49:54 +0700

    initial

---
+++ Assets/ClickAction.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickAction : MonoBehaviour
+{
+    public Texture2D cursorHover;
+    public CursorMode cursorMode = CursorMode.Auto;
+    public Vector2 hotSpot = Vector2.zero;
+
+    public GameObject currentState, nextState;
+    public Animator transition;
+
+    void OnMouseEnter()
+    {
+        Cursor.SetCursor(cursorHover, hotSpot, cursorMode);
+    }
+
+    void OnMouseExit()
+    {
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+    }
+
+    private void OnMouseDown()
+    {
+        StartCoroutine(ImageTransition());
+    }
+
+    IEnumerator ImageTransition()
+    {
+        
+        transition.SetTrigger("in");
+        yield return new WaitForSeconds(0.6f);
+        Cursor.SetCursor(null, Vector2.zero, cursorMode);
+        currentState.SetActive(false);
+        nextState.SetActive(true);
+    }
+}
